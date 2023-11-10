3b9c4f506d28b40f8881fa29d3c43078008fec8d
Atav <atav.daniyalov01@gmail.com>
2023-11-10 15:31:27 +0300

    sync

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,7 +3,8 @@ import store from '../store'
 import router from '../router'
 
 const axiosInstance = axios.create({
-  baseURL: 'http://10.11.58.67:8003/api/v1.0.0',
+  // baseURL: 'http://10.11.58.67:8003/api/v1.0.0',
+  baseURL: 'http://10.11.61.29:8001/api/v1.0.0',
   timeout: 10000
 })
 
@@ -15,11 +16,11 @@ axiosInstance.interceptors.request.use(
     }
     return config
   },
-  error => Promise.reject(error)
+  (error) => Promise.reject(error)
 )
 
 axiosInstance.interceptors.response.use(
-  response => response,
+  (response) => response,
   async function (error) {
     const originalRequest = error.config
     if (
@@ -29,10 +30,7 @@ axiosInstance.interceptors.response.use(
       store.commit('auth/clearUserData')
       router.push('/')
       return Promise.reject(error)
-    } else if (
-      error.response.status === 401 &&
-      !originalRequest._retry
-    ) {
+    } else if (error.response.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true
       await store.dispatch('auth/refreshToken')
 
